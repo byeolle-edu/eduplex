8fbd7a464c7aa7f881eae983b639771d14a5ace9
byeolle-edu <epx.015.eunbyul.park@gmail.com>
2026-08-09 23:14:53 +0900

    Update firebase-config.js

--- BN_Homepage/js/firebase-config.js
+++ BN_Homepage/js/firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyC2_Lm0KwXzrqDX5H4bDrnssEYh42LkV3Q",
   authDomain: "silverstar04301.firebaseapp.com",
   projectId: "silverstar04301",
